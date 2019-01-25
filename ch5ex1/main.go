5e5e9bfa8bd315b630b142f7f4c1507eace1248a
Vincent Smith <vosmith08@gmail.com>
2019-01-24 23:59:43 -0500

    fix for occasional broken links in Chapter 5 Example 1

--- ch5ex1/main.go
+++ ch5ex1/main.go
@@ -31,6 +31,9 @@ func main() {
 			description = strings.TrimSpace(bookPage.Find("div.book-top-block-info div.book-top-block-info-one-liner").Text())
 			price = strings.TrimSpace(bookPage.Find("div.book-top-block-info div.onlyDesktop div.book-top-pricing-main-ebook-price").Text())
 			authorNodes := bookPage.Find("div.book-top-block-info div.book-top-block-info-authors")
+			if len(authorNodes.Nodes) < 1 {
+				return
+			}
 			author = strings.TrimSpace(authorNodes.Nodes[0].FirstChild.Data)
 			fmt.Printf("%s\nby: %s\n%s\n%s\n---------------------\n\n", title, author, price, description)
 			time.Sleep(1 * time.Second)
