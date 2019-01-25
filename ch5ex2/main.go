a577819591d480f16d67663205a2f1e05616a325
Vincent Smith <vosmith08@gmail.com>
2019-01-25 00:23:56 -0500

    cleared error on Chapter 5 example 2

--- ch5ex2/main.go
+++ ch5ex2/main.go
@@ -10,6 +10,9 @@ func main() {
 	data.Set("s", "Golang")
 
 	response, err := http.PostForm("https://hub.packtpub.com/", data)
-	
+	if err != nil {
+		panic(err)
+	}
 	// ... Continue processing the response ...
+	println(response.StatusCode)
 }
